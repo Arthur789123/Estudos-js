2923557104f1d03b639fc2f27ff48667688ec42f
Alberto <betim009@gmail.com>
2024-04-25 12:02:39 -0300

    Novas tarefas

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Estudos-js
+
+
+`git clone` Faz o download do projeto/pasta
+
+`git add .` Faz alteração no projeto
+
+`git commit -m "mensagem"` Escreve o que foi feito na alteração
+
+`git push` Envia Pro github
+
+`git pull` Atualiza o projeto
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log ("lakers")
+const data = [
+    {
+        id: 1,
+        nome: "Brimstone",
+        funcao: "Controlador",
+        bigografia: "Vindo diretamente dos EUA, o arsenal orbital do Brimstone garante que o esquadrão dele sempre esteja em vantagem. Sua capacidade de oferecer utilitários com precisão e segurança faz dele um comandante inigualável na linha de frente."
+    },
+    {
+        id: 2,
+        funcao: "Duelista",
+        nome: "Phoenix",
+        bigografia: "Chegando com tudo diretamente do Reino Unido, o poder estelar de Phoenix reluz em seu estilo de luta, incendiando o campo de batalha com luz e estilo. Tendo ajuda ou não, ele entra na luta como e quando achar que deve."
+    },
+];
